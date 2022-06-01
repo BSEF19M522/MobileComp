552142644b30b5359a797638a2335f35df9aef2c
BSEF19M522 <bsef19m522@pucit.edu.pk>
2022-05-31 19:35:05 -0700

    ListView customized layout created

---
+++ CustomizedLisTview/app/src/main/java/com/example/customizedlistview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.customizedlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      listv=findViewById(R.id.listv);
+
+
+    }
+}
