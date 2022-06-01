f6f5939e7407f419de95e6f3dfeb51713972b09b
BSEF19M522 <bsef19m522@pucit.edu.pk>
2022-05-31 20:07:30 -0700

    ListView customized layout created images and data added

--- CustomizedLisTview/app/src/main/java/com/example/customizedlistview/MainActivity.java
+++ CustomizedLisTview/app/src/main/java/com/example/customizedlistview/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.customizedlistview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
       listv=findViewById(R.id.listv);
+      String [] textlink={"linkedIn", "GitHub", "BitBucket","Youtube"};
+      Integer[] images={R.drawable.linkedIn, R.drawable.GitHublogo,R.drawable.bitbucket,R.drawable.youtube};
 
+    }
+    public class CustomAdapter extends ArrayAdapter<String>{
 
+        public CustomAdapter(@NonNull Context context, int resource) {
+            super(context, resource);
+        }
     }
 }
