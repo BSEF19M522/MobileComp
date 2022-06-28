2f4599fdc467f294cb605ddbdb6db40abe37f7f7
BSEF19M522 <bsef19m522@pucit.edu.pk>
2022-06-27 18:38:51 -0700

    top scientists lists with their BIOGRAPHY through intent

--- Scientist_listListviewWithIntent/app/src/main/java/com/example/scientist_listlistviewwithintent/MainActivity.java
+++ Scientist_listListviewWithIntent/app/src/main/java/com/example/scientist_listlistviewwithintent/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import  android.content.Intent;
+import android.net.Uri;
+
 
 import java.util.ArrayList;
 
@@ -33,13 +36,52 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> ar_adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,scientists);
         MyList.setAdapter(ar_adapter);
-      /*  MyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        MyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String msg="Season" +i +" "+((TextView)view).getText().toString();
-                Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+             if(pos==0){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Albert_Einstein"));
+                 startActivity(i);
+             }
+             else if(pos==1){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Marie_Curie"));
+                 startActivity(i);
+             }
+             else if(pos==2){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Isaac_Newton"));
+                 startActivity(i);
+                }
+             else if(pos==3){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Charles_Darwin"));
+                 startActivity(i);
+                }
+             else if(pos==4){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Nikola_Tesla"));
+                 startActivity(i);
+                }
+             else if(pos==5){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Galileo_Galilei"));
+                 startActivity(i);
+                }
+             else if(pos==6){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Ada_Lovelace"));
+                 startActivity(i);
+                }
+             else if(pos==7){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Pythagoras"));
+                 startActivity(i);
+                }
+             else if(pos==8){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Carl_Linnaeus"));
+                 startActivity(i);
+                }
+             else if  (pos==9){
+                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Rosalind_Franklin"));
+                 startActivity(i);
+                }
+
             }
-        });*/
+        });
 
 
     }
