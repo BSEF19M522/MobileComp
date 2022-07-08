d75f2ab3439d825563126d842af7d9954139fc02
BSEF19M522 <bsef19m522@pucit.edu.pk>
2022-07-07 19:47:32 -0700

    counting layouts created using custom listview

--- counting/app/src/main/java/com/example/counting/MainActivity.java
+++ counting/app/src/main/java/com/example/counting/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.counting;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listv=findViewById(R.id.listv);
+
+
     }
 }
---
+++ counting/app/src/main/java/com/example/counting/appsclass.java
@@ -0,0 +1,27 @@
+package com.example.counting;
+
+public class appsclass {
+    int images;
+    String count;
+
+    public appsclass(int images, String count) {
+        this.images = images;
+        this.count = count;
+    }
+
+    public int getImages() {
+        return images;
+    }
+
+    public void setImages(int images) {
+        this.images = images;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+}
