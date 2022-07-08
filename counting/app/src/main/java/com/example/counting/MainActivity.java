b2e7c92541d41df2b05fe7f46b6459f1b8f18922
BSEF19M522 <bsef19m522@pucit.edu.pk>
2022-07-07 20:26:34 -0700

    counting from 1-10 created using custom listview

--- counting/app/src/main/java/com/example/counting/MainActivity.java
+++ counting/app/src/main/java/com/example/counting/MainActivity.java
@@ -2,26 +2,38 @@ package com.example.counting;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
-import android.view.View;
-import android.view.ViewGroup;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.view.LayoutInflater;
-import android.widget.TextView;
-
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     ListView listv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listv=findViewById(R.id.listv);
+        ArrayList<appsclass> arr= new ArrayList<>();
+        arr.add(new appsclass(R.drawable.one,"ONE"));
+        arr.add(new appsclass(R.drawable.two,"TWO"));
+        arr.add(new appsclass(R.drawable.three,"THREE"));
+        arr.add(new appsclass(R.drawable.four,"FOUR"));
+        arr.add(new appsclass(R.drawable.five,"FIVE"));
+        arr.add(new appsclass(R.drawable.six,"SIX"));
+        arr.add(new appsclass(R.drawable.seven,"SEVEN"));
+        arr.add(new appsclass(R.drawable.eight,"EIGHT"));
+        arr.add(new appsclass(R.drawable.nine,"NINE"));
+        arr.add(new appsclass(R.drawable.ten,"TEN"));
 
+
+
+
+
+        //add adapter class
+        countingAdapter aAdapt= new countingAdapter(this,R.layout.cuslist,arr);
+        listv.setAdapter(aAdapt);
 
     }
 }
