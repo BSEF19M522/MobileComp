a6c49b034d43e23f97df17f2d22b9550e439f0fb
BSEF19M522 <bsef19m522@pucit.edu.pk>
2022-07-27 01:23:59 -0700

    intentExtra task main activity2 added

---
+++ intentExtraTask/app/src/main/java/com/example/intentextratask/Activity2.java
@@ -0,0 +1,27 @@
+package com.example.intentextratask;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        Intent intent = getIntent();
+        String idd = intent.getStringExtra("t");
+        ImageView im;
+        TextView t1;
+        im=findViewById(R.id.imageView);
+        im.setImageResource(R.drawable.c);
+        t1=findViewById(R.id.textView);
+        t1.setText("HELLO");
+        hiiiiiii
+
+
+    }
+}
