b4e83f4d4f8e970d5aad5e78914494a0c2a194cd
BSEF19M522 <bsef19m522@pucit.edu.pk>
2022-08-17 19:42:57 -0700

    view with customlist 3

---
+++ DbResultsWithCustomlist/app/src/main/java/com/example/dbresultswithcustomlist/contactss.java
@@ -0,0 +1,35 @@
+package com.example.dbresultswithcustomlist;
+
+public class contactss {
+    private static String Name;
+    private static  String PhoneNumber;
+
+    public static String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public static String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public contactss(String name, String phoneNumber) {
+        Name = name;
+        PhoneNumber = phoneNumber;
+    }
+
+
+    @Override
+    public String toString() {
+
+        return "contactss{" + " Name='" + Name + '\'' + ", phoneNumber=" + PhoneNumber + '}';
+    }
+
+}
