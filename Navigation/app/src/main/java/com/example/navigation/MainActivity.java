f32f0a351a87912aecdb496fce16e474fcc80f90
BSEF19M522 <bsef19m522@pucit.edu.pk>
2022-08-27 22:23:12 -0700

    Navigation code app

--- Navigation/app/src/main/java/com/example/navigation/MainActivity.java
+++ Navigation/app/src/main/java/com/example/navigation/MainActivity.java
@@ -1,11 +1,30 @@
 package com.example.navigation;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
-import android.widget.Toolbar;
+
+
 
 public class MainActivity extends AppCompatActivity {
+private DrawerLayout drawerLayout;
+    Toolbar tb;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +32,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar tb=findViewById(R.id.tool);
         setSupportActionBar(tb);
+        drawerLayout=findViewById(R.id.Drawerlay);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this, drawerLayout,tb,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
 
 
     }
 
-    private void setSupportActionBar(Toolbar tb) {
-    }
+
+
 }
