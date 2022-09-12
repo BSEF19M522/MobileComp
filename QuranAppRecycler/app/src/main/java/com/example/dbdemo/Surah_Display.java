3077f382564642fe107cd629624dfd5b977d6b4f
BSEF19M522 <bsef19m522@pucit.edu.pk>
2022-09-11 20:29:41 -0700

    QuanApp Recycler 9

---
+++ QuranAppRecycler/app/src/main/java/com/example/dbdemo/Surah_Display.java
@@ -0,0 +1,66 @@
+/*package com.example.dbdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Surah_Display extends AppCompatActivity {
+    TextView textView;
+    ListView ayahs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surah_display);
+        Intent intent=getIntent();
+        int Surah_no=intent.getIntExtra("Surah_no",-1);
+        // textView=findViewById(R.id.textView);
+        //textView.setText(Integer.toString(Surah_no));
+        DatabaseAccess databaseAccess=DatabaseAccess.getInstance(getApplicationContext());
+        databaseAccess.open();
+        ayahs=findViewById(R.id.ayahs);
+        List_Adapter_Ayahs listadapter=new List_Adapter_Ayahs(this,databaseAccess.getSurahAyahs(Surah_no));
+        ayahs.setAdapter(listadapter);
+
+
+    }
+} */
+package com.example.dbdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Surah_Display extends AppCompatActivity {
+    TextView textView;
+    RecyclerView ayah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surah_display);
+        Intent intent = getIntent();
+        int Surah_no = intent.getIntExtra("Surah_no", -1);
+        // textView=findViewById(R.id.textView);
+        //textView.setText(Integer.toString(Surah_no));
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(getApplicationContext());
+        databaseAccess.open();
+        ayah = findViewById(R.id.ayahs);
+        ayah_recycle_view listadapter = new ayah_recycle_view(this, databaseAccess.getSurahAyahs(Surah_no));
+        ayah.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        ayah.setAdapter(listadapter);
+
+
+    }
+}
